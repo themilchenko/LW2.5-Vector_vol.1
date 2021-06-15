2d03eb6eadb3c93c5934c74065ea04d67153d411
Ivan <70635146+themilchenko@users.noreply.github.com>
2021-06-15 15:59:54 +0300

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -1,117 +1,88 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-
-#include "Vector.h"
 #include "VectorIterator.h"
+#include <stdexcept>
+#include <iostream>
 
-void print(int a)
+#pragma once
+
+class Vector
 {
-	std::cout << a << ' ';
-}
+	int* digits;
+	size_t _size;
+	size_t _capacity;
 
-bool greater(int a, int b)
-{
-	return a > b;
-}
+public:
+	Vector() : _size(0), _capacity(0) 
+	{
+		digits = new int[1];
+	};
 
-int main()
-{
+	Vector(const Vector& vec);
+	Vector(const int* vec, size_t num);
 
-	std::vector<int> vv = { 1, 2, 3, 4 };
-	vv.erase(vv.end() - 2, vv.end());
-	for (int i : vv)
-		std::cout << i << ' ';
+	void print()
+	{
+		for (int i : *this)
+			std::cout << i << ' ';
+		std::cout << std::endl;
+	}
 
-	int* a = new int[6];
-	
-	for (int i = 0; i < 6; ++i)
-		a[i] = i;
+	size_t size() const { return _size; };
+	size_t length() const { return _size; };
+	size_t capacity() const { return _capacity - 1; };
 
-	Vector vec(a, 6);
+	~Vector() 
+	{
+		if (digits != nullptr)
+			delete[] digits; 
+	};
 
-	std::cout << "Fifth element is " << vec.at(5) << std::endl;
-	vec.at(5) = 100;
-	std::cout << "Fifth element is " << vec.at(5) << std::endl;
+	Vector& operator=(const Vector& other);
+	VectorIterator begin() const { return VectorIterator(&digits[0]); };
+	VectorIterator end() const { return VectorIterator(&digits[_size]); };
 
-	std::cout << "Fifth element is " << vec[5] << std::endl;
-	vec[5] = 10000;
-	std::cout << "Fifth element is " << vec[5] << std::endl;
+	int& at(size_t index)
+	{
+		if ((index < _size) && (index >= 0))
+			return digits[index];
+		else
+			throw std::out_of_range("Out of range");
+	}
 
-	for (int i : vec)
-		std::cout << i << ' ';
-	std::cout << std::endl;
+	int& operator[](size_t index) { return digits[index]; };
 
-    //std::cout << *std::find(vec.begin(), vec.end(), 3) << std::endl;
+	int& front() { return *begin(); };
+	int& back() { return *(end() - 1); };
 
-	std::cout << "First element of container is " << vec.front() << std::endl;
-	std::cout << "Last element of container is " << vec.back() << std::endl;
-	std::cout << "Adress of container: " << vec.data() << std::endl;
-	std::cout << "Is container empty? - " << vec.empty() << std::endl;
-	std::cout << "The size of container is " << vec.size() << std::endl;
+	int* data() { return digits; };
 
-	std::cout << "Capacity: " << vec.capacity() << std::endl;
-	std::cout << "Capacity: " << vec.capacity() << std::endl;
-	vec.print();
-	vec.insert(vec.begin() + 3, 999);
-	vec.print();
-	vec.insert(vec.begin(), 876);
-	vec.print();
-	vec.insert(vec.end(), 12);
-	vec.print();
- 	vec.insert(vec.end() - 1, 13);
-	vec.print();
-	auto iter = vec.insert(vec.end() - 2, 21);
-	std::cout << *iter << std::endl;
-	vec.print();
+	bool empty() const 
+	{
+		if (_size == 0)
+			return true;
+		else
+			return false;
+	}
 
-	vec.insert(vec.begin() + 5, 1, 456);
-	vec.print();
+	void reserve(size_t num);
 
-	vec.clear();
-	vec.insert(vec.end(), 1);
-	vec.print();
- 	vec.insert(vec.end(), 99);
-	vec.print();
-	Vector vec2(a, 6);
-	vec.insert(vec.begin() + 1, vec2.begin(), vec2.end());
-	vec.print();
+	void clear() 
+	{
+		delete[] digits;
+		_size = 0;
 
-	vec2.insert(vec2.end(), vec.begin(), vec.end());
-	vec2.print();
-	vec.print();
+		digits = new int[1];
+	}
 
-	vec.erase(vec.begin() + 5);
-	vec.print();
+	VectorIterator insert(const VectorIterator& it, int&& element);
+	VectorIterator insert(const VectorIterator& it, size_t count, int&& element);
+	VectorIterator insert(const VectorIterator& _where, const VectorIterator& l_it, const VectorIterator& r_it);
 
-	vec.erase(vec.begin() + 1, vec.begin() + 3);
-	vec.print();
+	VectorIterator erase(const VectorIterator& it);
+	VectorIterator erase(const VectorIterator& l_it, const VectorIterator& r_it);
 
-	vec.erase(vec.end() - 2, vec.end());
-	vec.print();
+	void push_back(int&& element);
+	void pop_back() { _size--; };
 
-	vec.erase(vec.begin(), vec.end());
-	std::cout << vec.empty() << std::endl;
-
-	vec.insert(vec.begin(), 5, 100);
-	vec.print();
-
-	vec.push_back(1233);
-	vec.print();
-
-	vec.pop_back();
-
-	vec.resize(10);
-	vec.print();
-
-	vec.swap(vec2);
-	vec.print();
-	vec2.print();
-
-	std::for_each(vec.begin(), vec.end(), print);
-	std::find(vec.begin(), vec.end(), 100);
-	std::cout << std::count(vec.begin(), vec.end(), 1);
-	std::sort(vec.begin(), vec.end(), greater);
-	std::for_each(vec.begin(), vec.end(), print);
-	return 0;
-}
+	void resize(size_t actual_size);
+	void swap(Vector& other);
+};
